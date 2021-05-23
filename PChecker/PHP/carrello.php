ed9e12e2272d0877bef846534d0fd7475eeb9305
sorcho <mariobrosscot@yahoo.it>
2021-05-23 22:01:21 +0200

    pp eng

--- PChecker/PHP/carrello.php
+++ PChecker/PHP/carrello.php
@@ -89,7 +89,7 @@ if (isset($_POST['IDP'])) {
   <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
     <input type="hidden" name="cmd" value="_xclick">
     <input type="hidden" name="business" value="mattiascotellaro@yahoo.com">
-    <input type="hidden" name="lc" value="IT">
+    <input type="hidden" name="lc" value="US">
     <input type="hidden" name="button_subtype" value="services">
     <input type="hidden" name="no_note" value="1">
     <input type="hidden" name="no_shipping" value="2">
@@ -98,7 +98,7 @@ if (isset($_POST['IDP'])) {
     <input type="hidden" name="amount" value="<?php echo $danaro; ?>">
     <input type="hidden" name="item_name" value="Pagamento al sito PChecker">
     <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
-    <input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal è il metodo rapido e sicuro per pagare e farsi pagare online.">
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
     <img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
   </form>
 
