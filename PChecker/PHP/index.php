ab8d223e6b4cfec48608bc7209fee1216dbe5d88
sorcho <mariobrosscot@yahoo.it>
2021-05-25 02:21:26 +0200

    index

--- PChecker/PHP/index.php
+++ PChecker/PHP/index.php
@@ -20,10 +20,10 @@ session_start();
     <a href="contatti.php"><i class="fa fa-envelope" aria-hidden="true"></i> Contact me</a>
     <?php
     if (isset($_SESSION['email'])) {
-      if ($_SESSION['root'] == 0){
+      if ($_SESSION['root'] == 0) {
         echo "<a style='float: right;' href='utente.php'><i class='fa fa-user' aria-hidden='true'></i> " .  $_SESSION['nome'] . "</a>
               <a style='float: right;' href='carrello.php'><i class='fa fa-shopping-cart' aria-hidden='true'></i> Cart</a> ";
-      }else{
+      } else {
         echo "<a style='float: right;' href='admin.php'><i class='fa fa-user' aria-hidden='true'></i> " .  $_SESSION['nome'] . "</a>
               <a style='float: right;' href='carrello.php'><i class='fa fa-shopping-cart' aria-hidden='true'></i> Cart</a> ";
       }
@@ -33,19 +33,19 @@ session_start();
     ?>
   </div>
 
-  <center><img src="../IMG/io.jpeg" alt="letteralmente io"></center>
+  <center>
+    <img src="../IMG/logo.png" alt="logo">
+  </center>
 
-  <div class="footer">
-    <div>
-      <img class="logo" src="../IMG/logo.png" alt="PChecker">
-      <span style="visibility: hidden;vertical-align: middle;">l</span>
-    </div>
-    <div class="copyright">
-      <img style="visibility: hidden;" class="logo" src="../IMG/logo.png" alt="PChecker">
-      <span style="vertical-align: middle;">PChecker&copy; 2020/2021</span>
-    </div>
-    <div></div>
+  <div style="float: left; color: white; font-size: 35px;">
+    <p>PChecker is a website that allows every user to buy Hardware for their PC in an easy and intuitive way!<br>
+      You just have to register your user on the apposite Register tab, then Log-in and the job is done!<br>
+      PChecker borns from my passion for the PC world and Computer Science in general, this way I can merge my passions together.<br>
+      I thought that someone new can discover this website but may not know anything about these topic, so a good idea would be watching some
+      very good videos, the first will taught you how to build your own PC, the second will teach you something about coding in general.</p>
   </div>
+  <iframe width="750" height="500" style="margin-left: 20px ;float: left;" src="https://www.youtube.com/embed/v7MYOpFONCU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="750" height="500" style="margin-right: 20px ;float: right;" src="https://www.youtube.com/embed/RRubcjpTkks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
 
 </html>
